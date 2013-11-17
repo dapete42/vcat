ae06e5819da924e57784ece878a57dc4eea0595c
Peter Schlömer (dapete) <dev@dapete.net>
2013-11-17 21:17:09 +0100

    Category provider to get categories from Tool Labs DBs; should work, but untested

--- vcat-toollabs/src/main/java/vcat/toollabs/mediawiki/ToollabsCategoryProvider.java
+++ vcat-toollabs/src/main/java/vcat/toollabs/mediawiki/ToollabsCategoryProvider.java
@@ -16,6 +16,7 @@ import vcat.mediawiki.ICategoryProvider;
 import vcat.mediawiki.IMetadataProvider;
 import vcat.mediawiki.Metadata;
 import vcat.toollabs.ToollabsWiki;
+import vcat.util.CollectionHelper;
 
 public class ToollabsCategoryProvider implements ICategoryProvider<ToollabsWiki> {
 
@@ -27,6 +28,20 @@ public class ToollabsCategoryProvider implements ICategoryProvider<ToollabsWiki>
 			final IMetadataProvider metadataProvider) {
 		this.connectionBuilder = connectionBuilder;
 		this.metadataProvider = metadataProvider;
+	}
+
+	private static String preparedStatementInArguments(int numberOfArguments) {
+		StringBuilder sb = new StringBuilder(numberOfArguments * 2 + 1);
+		sb.append('(');
+		if (numberOfArguments > 0) {
+			sb.append('?');
+		}
+		for (int i = 1; i < numberOfArguments; i++) {
+			sb.append(',');
+			sb.append('?');
+		}
+		sb.append(')');
+		return sb.toString();
 	}
 
 	@Override
@@ -48,32 +63,62 @@ public class ToollabsCategoryProvider implements ICategoryProvider<ToollabsWiki>
 			throw new ApiException("Could not get connection for requestCategories", e);
 		}
 
-		final HashMap<String, Collection<String>> result = new HashMap<String, Collection<String>>(fullTitles.size());
-		for (final String fullTitle : fullTitles) {
+		// Get namespaces from full titles and store the truncated titles and full titles by namespace
+		final HashMap<Integer, ArrayList<String>> titlesByNamespace = new HashMap<Integer, ArrayList<String>>();
+		for (String fullTitle : fullTitles) {
 			final String title = metadata.titleWithoutNamespace(fullTitle);
 			final int namespace = metadata.namespaceFromTitle(fullTitle);
-			final ArrayList<String> categoryFullTitles = new ArrayList<String>();
-			try {
-				StringBuilder sql = new StringBuilder(
-						"SELECT cl_to FROM categorylinks INNER JOIN page ON page_id=cl_from"
-								+ " WHERE page_namespace=? AND page_title=?");
-				if (!showhidden) {
-					sql.append(" AND NOT EXISTS (SELECT * FROM page_props WHERE pp_page=page_id AND pp_propname='hiddencat');");
-				}
-				final PreparedStatement statement = connection.prepareStatement(sql.toString());
-				statement.setInt(1, namespace);
-				statement.setString(2, title);
-				ResultSet rs = statement.executeQuery();
-				while (rs.next()) {
-					final String cl_to = rs.getString("cl_to");
-					categoryFullTitles.add(metadata.fullTitle(cl_to, Metadata.NS_CATEGORY));
-				}
-				rs.close();
-			} catch (SQLException e) {
-				throw new ApiException("Error reading categories from Tool Labs database '" + dbname + '\'', e);
+			ArrayList<String> list = titlesByNamespace.get(namespace);
+			if (list == null) {
+				list = new ArrayList<String>(1);
+				titlesByNamespace.put(namespace, list);
 			}
-			if (!categoryFullTitles.isEmpty()) {
-				result.put(fullTitle, categoryFullTitles);
+			list.add(title);
+		}
+
+		final HashMap<String, Collection<String>> result = new HashMap<String, Collection<String>>(fullTitles.size());
+
+		for (int namespace : titlesByNamespace.keySet()) {
+			final ArrayList<String> allTitles = titlesByNamespace.get(namespace);
+
+			for (final Collection<String> titles : CollectionHelper.splitCollectionInParts(allTitles, 100)) {
+
+				try {
+					StringBuilder sql = new StringBuilder(
+							"SELECT page_title, cl_to FROM categorylinks INNER JOIN page ON page_id=cl_from"
+									+ " WHERE page_namespace=? AND page_title IN "
+									+ preparedStatementInArguments(titles.size()));
+					if (!showhidden) {
+						sql.append(" AND NOT EXISTS (SELECT * FROM page_props WHERE pp_page=page_id AND pp_propname='hiddencat');");
+					}
+					final PreparedStatement statement = connection.prepareStatement(sql.toString());
+					statement.setInt(1, namespace);
+					int i = 2;
+					for (String title : titles) {
+						statement.setString(i, title);
+						i++;
+					}
+					ResultSet rs = statement.executeQuery();
+					while (rs.next()) {
+						// We need the page_title because we get results for different pages
+						final String page_title = rs.getString("page_title");
+						final String cl_to = rs.getString("cl_to");
+						// This is the full title we got the categories for
+						final String fullTitle = metadata.fullTitle(page_title, namespace);
+						// This is the full title of the categories themselves
+						final String categoryFullTitle = metadata.fullTitle(cl_to, Metadata.NS_CATEGORY);
+						// Put title in results at correct fullTitle key
+						Collection<String> categoryFullTitles = result.get(fullTitle);
+						if (categoryFullTitles == null) {
+							categoryFullTitles = new ArrayList<String>(1);
+							result.put(fullTitle, categoryFullTitles);
+						}
+						categoryFullTitles.add(categoryFullTitle);
+					}
+					rs.close();
+				} catch (SQLException e) {
+					throw new ApiException("Error reading categories from Tool Labs database '" + dbname + '\'', e);
+				}
 			}
 		}
 
