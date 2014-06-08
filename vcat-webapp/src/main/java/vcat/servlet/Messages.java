c820f7340e73dff54503b8398c45c3991dbe90ce
Peter Schlömer (dapete) <dev@dapete.net>
2014-06-08 18:50:34 +0200

    Cleanup

--- vcat-webapp/src/main/java/vcat/servlet/Messages.java
+++ vcat-webapp/src/main/java/vcat/servlet/Messages.java
@@ -9,9 +9,6 @@ public class Messages {
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
-	private Messages() {
-	}
-
 	public static String getString(String key) {
 		try {
 			return RESOURCE_BUNDLE.getString(key);
