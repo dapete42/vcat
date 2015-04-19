6ea4f0227bd25c3ac70068253866bf4e3d21653d
Peter Schlömer (dapete) <dev@dapete.net>
2015-04-19 11:41:26 +0200

    Add compatibility for new continue process which will become default in MediaWiki 1.26

--- vcat-core/src/main/java/vcat/mediawiki/ApiClient.java
+++ vcat-core/src/main/java/vcat/mediawiki/ApiClient.java
@@ -37,6 +37,28 @@ public class ApiClient<W extends IWiki> implements ICategoryProvider<W>, IMetada
 		this.client.getParams().setParameter("User-Agent", Messages.getString("ApiClient.UserAgent"));
 	}
 
+	private static Map<String, String> buildContinueMap(JsonObject result, final String oldQueryContinueChildName) {
+		Map<String, String> continueMap = null;
+		final boolean containsContinue = result.containsKey("continue");
+		if (containsContinue || result.containsKey("query-continue")) {
+			JsonObject jsonContinue = null;
+			if (containsContinue) {
+				// new continue (default starting with MW 1.26)
+				jsonContinue = result.getJsonObject("continue");
+			} else {
+				// Old "raw" continue (default until MW 1.25)
+				jsonContinue = result.getJsonObject("query-continue").getJsonObject(oldQueryContinueChildName);
+			}
+			if (jsonContinue != null) {
+				continueMap = new HashMap<>();
+				for (String key : jsonContinue.keySet()) {
+					continueMap.put(key, jsonContinue.getString(key));
+				}
+			}
+		}
+		return continueMap;
+	}
+
 	protected JsonObject request(String apiUrl, Map<String, String> params) throws ApiException {
 		// API Etiquette: use get to enable server-side caching
 		HttpRequestBuilder builder = Http.get(apiUrl);
@@ -67,61 +89,65 @@ public class ApiClient<W extends IWiki> implements ICategoryProvider<W>, IMetada
 	@Override
 	public Map<String, Collection<String>> requestCategories(W wiki, Collection<String> fullTitles, boolean showhidden)
 			throws ApiException {
-		HashMap<String, Collection<String>> categoryMap = new HashMap<>();
+		Map<String, Collection<String>> categoryMap = new HashMap<>();
 		String clshow = showhidden ? null : "!hidden";
 		this.requestCategoriesRecursive(wiki.getApiUrl(), fullTitles, categoryMap, null, clshow);
 		return categoryMap;
 	}
 
 	private void requestCategoriesRecursive(String apiUrl, Collection<String> fullTitles,
-			final Map<String, Collection<String>> categoryMap, String clcontinue, String clshow) throws ApiException {
+			final Map<String, Collection<String>> categoryMap, Map<String, String> continueMap, String clshow)
+			throws ApiException {
 
 		if (fullTitles.size() > TITLES_MAX) {
 
 			for (Collection<String> fullTitlesPart : CollectionHelper.splitCollectionInParts(fullTitles, TITLES_MAX)) {
-				requestCategoriesRecursive(apiUrl, fullTitlesPart, categoryMap, clcontinue, clshow);
+				requestCategoriesRecursive(apiUrl, fullTitlesPart, categoryMap, continueMap, clshow);
 			}
 
 		} else {
 
 			// Set query properties
-			HashMap<String, String> params = new HashMap<>();
+			Map<String, String> params = new HashMap<>();
 			params.put("prop", "categories");
 			params.put("cllimit", "max");
 			params.put("titles", StringUtils.join(fullTitles, '|'));
 			if (clshow != null) {
 				params.put("clshow", clshow);
 			}
-			if (clcontinue != null) {
-				params.put("clcontinue", clcontinue);
+			if (continueMap != null) {
+				params.putAll(continueMap);
 			}
 			JsonObject result = this.request(apiUrl, params);
 
 			try {
 				JsonObject query = result.getJsonObject("query");
-				JsonObject pages = query.getJsonObject("pages");
-				for (String pagesKey : pages.keySet()) {
-					JsonObject pagesData = pages.getJsonObject(pagesKey);
-					if (pagesData.containsKey("categories")) {
-						JsonArray jsonCategories = pagesData.getJsonArray("categories");
-						String pagesDataTitle = pagesData.getString("title");
-						Collection<String> categories = categoryMap.get(pagesDataTitle);
-						if (categories == null) {
-							categories = new ArrayList<>(jsonCategories.size());
-							categoryMap.put(pagesDataTitle, categories);
-						}
-						for (int i = 0; i < jsonCategories.size(); i++) {
-							JsonObject category = jsonCategories.getJsonObject(i);
-							categories.add(category.getString("title"));
+				if (query != null) {
+					JsonObject pages = query.getJsonObject("pages");
+					if (pages != null) {
+						for (String pagesKey : pages.keySet()) {
+							JsonObject pagesData = pages.getJsonObject(pagesKey);
+							if (pagesData.containsKey("categories")) {
+								JsonArray jsonCategories = pagesData.getJsonArray("categories");
+								String pagesDataTitle = pagesData.getString("title");
+								Collection<String> categories = categoryMap.get(pagesDataTitle);
+								if (categories == null) {
+									categories = new ArrayList<>(jsonCategories.size());
+									categoryMap.put(pagesDataTitle, categories);
+								}
+								for (int i = 0; i < jsonCategories.size(); i++) {
+									JsonObject category = jsonCategories.getJsonObject(i);
+									categories.add(category.getString("title"));
+								}
+							}
 						}
 					}
 				}
 
-				// If query-continue was returned, make another request
-				if (result.containsKey("query-continue")) {
-					this.requestCategoriesRecursive(apiUrl, fullTitles, categoryMap,
-							result.getJsonObject("query-continue").getJsonObject("categories").getString("clcontinue"),
-							clshow);
+				// Try to get continue map, if there is one, make another request
+				final Map<String, String> newContinueMap = buildContinueMap(result, "categories");
+				if (newContinueMap != null) {
+					this.requestCategoriesRecursive(apiUrl, fullTitles, categoryMap, newContinueMap, clshow);
 				}
 			} catch (JsonException e) {
 				throw new ApiException(Messages.getString("ApiClient.Exception.ParsingJSON"), e);
@@ -138,33 +164,35 @@ public class ApiClient<W extends IWiki> implements ICategoryProvider<W>, IMetada
 	}
 
 	private void requestCategorymembersRecursive(final String apiUrl, final String fullTitle,
-			final List<String> categories, final String cmcontinue) throws ApiException {
+			final List<String> categories, final Map<String, String> continueMap) throws ApiException {
 
 		// Set query properties
-		HashMap<String, String> params = new HashMap<>();
+		Map<String, String> params = new HashMap<>();
 		params.put("list", "categorymembers");
 		params.put("cmlimit", "max");
 		params.put("cmtitle", fullTitle);
 		params.put("cmtype", "subcat");
-		if (cmcontinue != null) {
-			params.put("cmcontinue", cmcontinue);
+		if (continueMap != null) {
+			params.putAll(continueMap);
 		}
 		JsonObject result = this.request(apiUrl, params);
 
 		try {
 			JsonObject query = result.getJsonObject("query");
-			if (query.containsKey("categorymembers")) {
+			if (query != null) {
 				JsonArray jsonCategories = query.getJsonArray("categorymembers");
-				for (int i = 0; i < jsonCategories.size(); i++) {
-					JsonObject category = jsonCategories.getJsonObject(i);
-					categories.add(category.getString("title"));
+				if (jsonCategories != null) {
+					for (int i = 0; i < jsonCategories.size(); i++) {
+						JsonObject category = jsonCategories.getJsonObject(i);
+						categories.add(category.getString("title"));
+					}
 				}
 			}
 
-			// If query-continue was returned, make another request
-			if (result.containsKey("query-continue")) {
-				this.requestCategorymembersRecursive(apiUrl, fullTitle, categories,
-						result.getJsonObject("query-continue").getJsonObject("categorymembers").getString("cmcontinue"));
+			// Try to get continue map, if there is one, make another request
+			final Map<String, String> newContinueMap = buildContinueMap(result, "categorymembers");
+			if (newContinueMap != null) {
+				this.requestCategorymembersRecursive(apiUrl, fullTitle, categories, newContinueMap);
 			}
 		} catch (JsonException e) {
 			throw new ApiException(Messages.getString("ApiClient.Exception.ParsingJSON"), e);
@@ -179,48 +207,50 @@ public class ApiClient<W extends IWiki> implements ICategoryProvider<W>, IMetada
 		return links;
 	}
 
-	private void requestLinksBetweenRecursive(String apiUrl, Collection<String> fullTitles,
-			final Collection<Pair<String, String>> links, String plcontinue) throws ApiException {
+	private void requestLinksBetweenRecursive(final String apiUrl, final Collection<String> fullTitles,
+			final Collection<Pair<String, String>> links, Map<String, String> continueMap) throws ApiException {
 
 		if (fullTitles.size() > TITLES_MAX) {
 
 			for (Collection<String> fullTitlesPart : CollectionHelper.splitCollectionInParts(fullTitles, TITLES_MAX)) {
-				requestLinksBetweenRecursive(apiUrl, fullTitlesPart, links, plcontinue);
+				requestLinksBetweenRecursive(apiUrl, fullTitlesPart, links, continueMap);
 			}
 
 		} else {
 
 			// Set query properties
-			HashMap<String, String> params = new HashMap<>();
+			Map<String, String> params = new HashMap<>();
 			params.put("prop", "links");
 			params.put("pllimit", "max");
 			final String titlesParam = StringUtils.join(fullTitles, '|');
 			params.put("titles", titlesParam);
 			params.put("pltitles", titlesParam);
-			if (plcontinue != null) {
-				params.put("plcontinue", plcontinue);
+			if (continueMap != null) {
+				params.putAll(continueMap);
 			}
 			JsonObject result = this.request(apiUrl, params);
 
 			try {
 				JsonObject query = result.getJsonObject("query");
-				JsonObject pages = query.getJsonObject("pages");
-				for (String pagesKey : pages.keySet()) {
-					JsonObject pagesData = pages.getJsonObject(pagesKey);
-					if (pagesData.containsKey("links")) {
-						JsonArray jsonLinks = pagesData.getJsonArray("links");
-						String pagesDataTitle = pagesData.getString("title");
-						for (int i = 0; i < jsonLinks.size(); i++) {
-							JsonObject category = jsonLinks.getJsonObject(i);
-							links.add(new MutablePair<>(pagesDataTitle, category.getString("title")));
+				if (query != null) {
+					JsonObject pages = query.getJsonObject("pages");
+					for (String pagesKey : pages.keySet()) {
+						JsonObject pagesData = pages.getJsonObject(pagesKey);
+						if (pagesData.containsKey("links")) {
+							JsonArray jsonLinks = pagesData.getJsonArray("links");
+							String pagesDataTitle = pagesData.getString("title");
+							for (int i = 0; i < jsonLinks.size(); i++) {
+								JsonObject category = jsonLinks.getJsonObject(i);
+								links.add(new MutablePair<>(pagesDataTitle, category.getString("title")));
+							}
 						}
 					}
 				}
 
-				// If query-continue was returned, make another request
-				if (result.containsKey("query-continue")) {
-					this.requestLinksBetweenRecursive(apiUrl, fullTitles, links, result.getJsonObject("query-continue")
-							.getJsonObject("categories").getString("plcontinue"));
+				// Try to get continue map, if there is one, make another request
+				final Map<String, String> newContinueMap = buildContinueMap(result, "categories");
+				if (newContinueMap != null) {
+					this.requestLinksBetweenRecursive(apiUrl, fullTitles, links, newContinueMap);
 				}
 			} catch (JsonException e) {
 				throw new ApiException(Messages.getString("ApiClient.Exception.ParsingJSON"), e);
@@ -232,7 +262,7 @@ public class ApiClient<W extends IWiki> implements ICategoryProvider<W>, IMetada
 	@Override
 	public Metadata requestMetadata(final IWiki wiki) throws ApiException {
 		// Set query properties
-		HashMap<String, String> params = new HashMap<>();
+		Map<String, String> params = new HashMap<>();
 		params.put("meta", "siteinfo");
 		params.put("siprop", "general|namespaces|namespacealiases");
 
---
+++ vcat-core/src/test/java/vcat/mediawiki/ApiClientTest.java
@@ -0,0 +1,69 @@
+package vcat.mediawiki;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ApiClientTest {
+
+	/* Use German Wikipedia for testing. */
+
+	private final static String PAGE_1 = "Deutschland";
+
+	private final static String PAGE_2 = "Frankreich";
+
+	private final static String CATEGORY_1 = "Kategorie:Deutschland";
+
+	private final static String WIKI_API = "http://de.wikipedia.org/w/api.php";
+
+	private IWiki wiki;
+
+	private ApiClient<IWiki> client;
+
+	@Before
+	public void before() {
+		this.wiki = new SimpleWiki("test", WIKI_API);
+		this.client = new ApiClient<>();
+	}
+
+	@Test
+	public void testRequestCategories() throws ApiException {
+		final List<String> fullTitles = new ArrayList<>();
+		fullTitles.add(PAGE_1);
+		fullTitles.add(PAGE_2);
+		final Map<String, Collection<String>> result = this.client.requestCategories(wiki, fullTitles, true);
+		assertNotNull(result);
+		assertNotEquals(0, result.size());
+	}
+
+	@Test
+	public void testRequestCategorymembers() throws ApiException {
+		final List<String> result = this.client.requestCategorymembers(wiki, CATEGORY_1);
+		assertNotNull(result);
+		assertNotEquals(0, result.size());
+	}
+
+	@Test
+	public void testRequestLinksBetween() throws ApiException {
+		final List<String> fullTitles = new ArrayList<>();
+		fullTitles.add(PAGE_1);
+		fullTitles.add(PAGE_2);
+		final Collection<Pair<String, String>> result = client.requestLinksBetween(wiki, fullTitles);
+		assertNotNull(result);
+		assertNotEquals(0, result.size());
+	}
+
+	@Test
+	public void testRequestMetadata() throws ApiException {
+		Metadata result = client.requestMetadata(wiki);
+		assertNotNull(result);
+	}
+
+}
