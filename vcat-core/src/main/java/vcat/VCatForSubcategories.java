004ea6abfedf863ee2770e05bea90d69d43b8897
Peter Schlömer (dapete) <dev@dapete.net>
2023-11-11 17:05:25 +0100

    Refactoring: generalize and split off inner loop in VCat implementations

--- vcat-core/src/main/java/vcat/VCatForSubcategories.java
+++ vcat-core/src/main/java/vcat/VCatForSubcategories.java
@@ -1,6 +1,5 @@
 package vcat;
 
-import vcat.graph.Edge;
 import vcat.graph.Graph;
 import vcat.graph.GroupRank;
 import vcat.graph.Node;
@@ -9,11 +8,8 @@ import vcat.mediawiki.interfaces.CategoryProvider;
 import vcat.mediawiki.interfaces.Wiki;
 import vcat.params.AbstractAllParams;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.Set;
-import java.util.function.BiFunction;
 
 public class VCatForSubcategories<W extends Wiki> extends AbstractVCat<W> {
 
@@ -21,8 +17,9 @@ public class VCatForSubcategories<W extends Wiki> extends AbstractVCat<W> {
         super(all, categoryProvider);
     }
 
-    protected void renderGraphInnerLoop(Graph graph, Node rootNode, Set<Node> allNodesFound, Collection<Node> newNodes,
-                                        Collection<String> categoryFullTitles, String categoryNamespacePrefix) {
+    protected void renderGraphInnerLoop(
+            Graph graph, Node rootNode, Set<Node> allNodesFound, Collection<Node> newNodes,
+            Collection<String> categoryFullTitles, String categoryNamespacePrefix) {
         for (String categoryFullTitle : categoryFullTitles) {
             String categoryTitle = categoryFullTitle.substring(categoryNamespacePrefix.length());
             Node categoryNode = graph.node(categoryTitle);
@@ -36,10 +33,9 @@ public class VCatForSubcategories<W extends Wiki> extends AbstractVCat<W> {
     }
 
     @Override
-    protected void renderGraphOuterFirstLoop(Graph graph, Collection<Node> newNodes, Node rootNode,
-                                             Set<Node> allNodesFound, String fullTitle, String categoryNamespacePrefix,
-                                             boolean showHidden)
-            throws ApiException {
+    protected void renderGraphOuterFirstLoop(
+            Graph graph, Collection<Node> newNodes, Node rootNode, Set<Node> allNodesFound, String fullTitle,
+            String categoryNamespacePrefix, boolean showHidden) throws ApiException {
         Collection<String> categoryFullTitles = categoryProvider.requestCategorymembers(all.getWiki(), fullTitle);
         if (categoryFullTitles != null) {
             renderGraphInnerLoop(graph, rootNode, allNodesFound, newNodes, categoryFullTitles, categoryNamespacePrefix);
@@ -47,22 +43,16 @@ public class VCatForSubcategories<W extends Wiki> extends AbstractVCat<W> {
     }
 
     @Override
-    protected void renderGraphOuterLoop(Graph graph, Collection<Node> newNodes, Collection<Node> curNodes,
-                                        Set<Node> allNodesFound, String categoryNamespacePrefix,
-                                        boolean showhidden, boolean exceedDepth) throws ApiException {
+    protected void renderGraphOuterLoop(
+            Graph graph, Collection<Node> newNodes, Collection<Node> curNodes, Set<Node> allNodesFound,
+            String categoryNamespacePrefix, boolean showHidden, boolean exceedDepth) throws ApiException {
 
-        // Create a list of the full titles (including namespace) of the categories in the current loop iteration
-        var curFullTitles = curNodes.stream()
-                .map(Node::getName)
-                .map(name -> categoryNamespacePrefix + name)
-                .toList();
+        final CreateEdgeFunction createEdgeFunction = graph::edgeReverse;
 
-        final BiFunction<Node, Node, Edge> createEdgeFunction = graph::edgeReverse;
-
-        for (String fullTitle : curFullTitles) {
-            final var categoryFullTitles = categoryProvider.requestCategorymembers(all.getWiki(), fullTitle);
-            // For each API result, first get the node it contains categories for
-            final String baseTitle = fullTitle.substring(categoryNamespacePrefix.length());
+        for (var curNode : curNodes) {
+            final var categoryFullTitles = categoryProvider
+                    .requestCategorymembers(all.getWiki(), categoryNamespacePrefix + curNode.getName());
+            final String baseTitle = curNode.getName();
             renderGraphInnerLoop(graph, newNodes, allNodesFound, categoryNamespacePrefix, exceedDepth, baseTitle,
                     categoryFullTitles, createEdgeFunction);
         }
