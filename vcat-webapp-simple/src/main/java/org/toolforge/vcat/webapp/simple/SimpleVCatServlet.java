af84295d46f91778f2784052399c54508017c829
Peter Schlömer (dapete) <dev@dapete.net>
2023-11-19 11:13:09 +0100

    Refactor SimpleVCatServlet

--- vcat-webapp-simple/src/main/java/org/toolforge/vcat/webapp/simple/SimpleVCatServlet.java
+++ vcat-webapp-simple/src/main/java/org/toolforge/vcat/webapp/simple/SimpleVCatServlet.java
@@ -1,10 +1,15 @@
 package org.toolforge.vcat.webapp.simple;
 
 import jakarta.inject.Inject;
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.toolforge.vcat.VCatException;
 import org.toolforge.vcat.cache.interfaces.MetadataCache;
@@ -20,17 +25,21 @@ import org.toolforge.vcat.renderer.CachedVCatRenderer;
 import org.toolforge.vcat.renderer.QueuedVCatRenderer;
 import org.toolforge.vcat.renderer.RenderedFileInfo;
 import org.toolforge.vcat.renderer.interfaces.VCatRenderer;
-import org.toolforge.vcat.webapp.base.AbstractVCatServlet;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serial;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @WebServlet(urlPatterns = "/render")
-public class SimpleVCatServlet extends AbstractVCatServlet {
+@Slf4j
+public class SimpleVCatServlet extends HttpServlet {
 
     @Serial
-    private static final long serialVersionUID = -6050180232379416509L;
+    private static final long serialVersionUID = 7822892157349895078L;
 
     @Inject
     @ConfigProperty(name = "graphviz.dir", defaultValue = "/usr/bin")
@@ -64,9 +73,9 @@ public class SimpleVCatServlet extends AbstractVCatServlet {
     @ConfigProperty(name = "vcat.threads", defaultValue = "0")
     Integer vcatThreads;
 
-    private static MetadataProvider metadataProvider;
+    private MetadataProvider metadataProvider;
 
-    private static VCatRenderer vCatRenderer;
+    private VCatRenderer vCatRenderer;
 
     @Override
     public String getServletInfo() {
@@ -74,10 +83,19 @@ public class SimpleVCatServlet extends AbstractVCatServlet {
     }
 
     @Override
-    public void init() throws ServletException {
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
         final var cachePath = Paths.get(cacheDir);
-        final var tempDir = ((File) this.getServletContext().getAttribute(ServletContext.TEMPDIR)).toPath();
+        LOG.info("Using cache directory {}", cachePath);
+        final var tempdirServletContextAttribute = getServletContext().getAttribute(ServletContext.TEMPDIR);
         try {
+            final Path tempDir;
+            if (tempdirServletContextAttribute instanceof File tempDirFile) {
+                tempDir = tempDirFile.toPath();
+            } else {
+                tempDir = Files.createTempDirectory("vcat-webapp-simple");
+            }
+            LOG.info("Using temporary directory {}", tempDir);
             final var graphviz = new QueuedGraphviz(new GraphvizExternal(Paths.get(graphvizDir)), 1);
             final var apiCache = new ApiCaffeineCache(10000, cachePurge);
             final CachedApiClient apiClient = new CachedApiClient(apiCache);
@@ -85,16 +103,51 @@ public class SimpleVCatServlet extends AbstractVCatServlet {
             metadataProvider = new CachedMetadataProvider(apiClient, metadataCache);
             vCatRenderer = new QueuedVCatRenderer(
                     new CachedVCatRenderer(graphviz, tempDir, apiClient, cachePath, cachePurge), vcatThreads);
-        } catch (VCatException e) {
+        } catch (IOException | VCatException e) {
             throw new ServletException(e);
         }
     }
 
     @Override
-    protected RenderedFileInfo renderedFileFromRequest(final HttpServletRequest req) throws ServletException {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
+        this.doRequest(req, resp);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
+        this.doRequest(req, resp);
+    }
+
+    protected void doRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
         try {
-            return vCatRenderer.render(new AllParams(req.getParameterMap(), req.getRequestURI(), metadataProvider));
-        } catch (VCatException e) {
+            RenderedFileInfo renderedFileInfo = vCatRenderer.render(
+                    new AllParams(req.getParameterMap(), req.getRequestURI(), metadataProvider));
+
+            // Get finished rendered file
+            Path resultFile = renderedFileInfo.getFile();
+
+            // Content-type
+            String contentType = renderedFileInfo.getMimeType();
+            resp.setContentType(contentType);
+
+            // Content-length
+            long length = Files.size(resultFile);
+            if (length < Integer.MAX_VALUE) {
+                resp.setContentLength((int) length);
+            }
+
+            // Content-disposition (for file name)
+            String filename = resultFile.getFileName().toString();
+            resp.setHeader("Content-disposition", "filename=\"" + filename + '"');
+
+            // Serve file to browser
+            try (InputStream renderedInput = Files.newInputStream(resultFile);
+                 ServletOutputStream output = resp.getOutputStream()) {
+                renderedInput.transferTo(output);
+            }
+
+            LOG.info("File sent: '{}' sent as '{}', {} bytes", resultFile.toAbsolutePath(), contentType, length);
+        } catch (Exception e) {
             throw new ServletException(e);
         }
     }
