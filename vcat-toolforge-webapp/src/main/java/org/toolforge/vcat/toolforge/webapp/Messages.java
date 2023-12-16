523f6cb2147a47d89759adc8c8f0636925c1ad36
Peter Schlömer (dapete) <dev@dapete.net>
2023-12-16 19:39:19 +0100

    Remove Catgraph converter

--- vcat-toolforge-webapp/src/main/java/org/toolforge/vcat/toolforge/webapp/Messages.java
+++ vcat-toolforge-webapp/src/main/java/org/toolforge/vcat/toolforge/webapp/Messages.java
@@ -9,32 +9,11 @@ public class Messages {
 
     private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
-    private static final String RESOURCE_PREFIX_CATGRAPHCONVERTER = "ToolforgeCatgraphConverterServlet.String.";
-
     public static String getString(String key) {
         try {
             return RESOURCE_BUNDLE.getString(key);
         } catch (MissingResourceException e) {
             return '!' + key + '!';
-        }
-    }
-
-    private final String lang;
-
-    public Messages(String lang) {
-        this.lang = lang;
-    }
-
-    public String getCatgraphConverterString(String key) {
-        // ToolforgeCatgraphConverterServlet.String.de.title
-        try {
-            return RESOURCE_BUNDLE.getString(RESOURCE_PREFIX_CATGRAPHCONVERTER + this.lang + '.' + key);
-        } catch (MissingResourceException e1) {
-            try {
-                return RESOURCE_BUNDLE.getString(RESOURCE_PREFIX_CATGRAPHCONVERTER + "en." + key);
-            } catch (MissingResourceException e2) {
-                return '!' + key + '!';
-            }
         }
     }
 
