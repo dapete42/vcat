ca62da12573ffba2d0a7c392b77005fb3e8e1c4d
Peter Schlömer (dapete) <dev@dapete.net>
2024-12-08 11:02:47 +0100

    Fix NullPointerException when using ReentrantLocks
    In production, the Lock returned by the LockReference is sometimes null. Probably a timing issue with the garbage collector.
    
    This is now handled by creating a new LockReference if the Lock was null.

--- vcat-core/src/main/java/org/toolforge/vcat/util/ReentrantLocks.java
+++ vcat-core/src/main/java/org/toolforge/vcat/util/ReentrantLocks.java
@@ -2,9 +2,8 @@ package org.toolforge.vcat.util;
 
 import java.lang.ref.ReferenceQueue;
 import java.lang.ref.WeakReference;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -30,11 +29,11 @@ public class ReentrantLocks<K> {
 
     }
 
-    private final Map<K, LockReference> lockReferenceMap = new ConcurrentHashMap<>();
+    private final Lock lock = new ReentrantLock();
+
+    private final Map<K, LockReference> lockReferenceMap = new HashMap<>();
 
     private final ReferenceQueue<ReentrantLock> lockReferenceQueue = new ReferenceQueue<>();
-
-    private final Lock lockReferenceQueueLock = new ReentrantLock();
 
     /**
      * Return a lock already locked using {@link Lock#lock()}.
@@ -44,9 +43,7 @@ public class ReentrantLocks<K> {
      */
     public ReentrantLock lock(K key) {
         processQueue();
-        final var lock = Objects.requireNonNull(
-                newLock(key)
-        );
+        final var lock = getLock(key);
         lock.lock();
         return lock;
     }
@@ -58,18 +55,29 @@ public class ReentrantLocks<K> {
      */
     public int size() {
         processQueue();
-        return lockReferenceMap.size();
+        lock.lock();
+        try {
+            return lockReferenceMap.size();
+        } finally {
+            lock.unlock();
+        }
     }
 
-    private ReentrantLock newLock(K key) {
-        return Objects.requireNonNull(
-                lockReferenceMap.computeIfAbsent(key, this::newLockReference).get()
-        );
-    }
-
-    private LockReference newLockReference(K key) {
-        final var lock = new ReentrantLock();
-        return new LockReference(key, lock);
+    private ReentrantLock getLock(K key) {
+        lock.lock();
+        try {
+            final var lockReference = lockReferenceMap.get(key);
+            final var lock = lockReference == null ? null : lockReference.get();
+            if (lock == null) {
+                final var newLock = new ReentrantLock();
+                lockReferenceMap.put(key, new LockReference(key, newLock));
+                return newLock;
+            } else {
+                return lock;
+            }
+        } finally {
+            lock.unlock();
+        }
     }
 
     /**
@@ -77,14 +85,14 @@ public class ReentrantLocks<K> {
      */
     @SuppressWarnings("unchecked")
     private void processQueue() {
-        lockReferenceQueueLock.lock();
+        lock.lock();
         try {
             LockReference lockReference;
             while ((lockReference = (LockReference) lockReferenceQueue.poll()) != null) {
                 lockReferenceMap.remove(lockReference.getKey());
             }
         } finally {
-            lockReferenceQueueLock.unlock();
+            lock.unlock();
         }
     }
 
